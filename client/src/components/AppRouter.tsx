cf07e0e92278e07b003ff96a25b61eef6d10530e
Alexei <lexas427@gmail.com>
2022-07-23 19:35:48 +0300

    feat: implement routes

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes/routes';
 
 const AppRouter = () => {
-  return <div></div>;
+  const isAuth = false;
+  return (
+    <Routes>
+      <>
+        {isAuth &&
+          authRoutes.map(({ path, Component }) => {
+            return <Route key={path} path={path} element={<Component />} />;
+          })}
+        {publicRoutes.map(({ path, Component }) => {
+          return <Route key={path} path={path} element={<Component />} />;
+        })}
+      </>
+    </Routes>
+  );
 };
 
 export default AppRouter;
--- client/src/pages/DevicePage.tsx
+++ client/src/pages/DevicePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DevicePage = () => {
-  return <div></div>;
+  return <div>device</div>;
 };
 
 export default DevicePage;
